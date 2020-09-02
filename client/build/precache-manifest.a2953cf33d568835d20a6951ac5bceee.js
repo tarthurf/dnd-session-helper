d0b93fc037219291b3fb486514f0275dcca33df3
Swedish <tarthurf@yahoo.com>
2020-09-02 11:22:46 -0700

    created build folder

---
+++ client/build/precache-manifest.a2953cf33d568835d20a6951ac5bceee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fe0c40726da5796d30bb",
+    "url": "/static/css/main.75188a07.chunk.css"
+  },
+  {
+    "revision": "fe0c40726da5796d30bb",
+    "url": "/static/js/main.c156522b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7e5980ba7658cdb4dfd1",
+    "url": "/static/js/2.acb841aa.chunk.js"
+  },
+  {
+    "revision": "9c45699352f062a433e80647475ba761",
+    "url": "/index.html"
+  }
+];
