b14448559791ace8281901ab9d2a9f1214668b71
Swedish <tarthurf@yahoo.com>
2020-09-19 13:18:13 -0700

    come on, do something

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/dnd-session-helper.git/precache-manifest.dc8cb5df0cbb9b5bc3e4735808f1f63e.js"
+  "/precache-manifest.b1458422d1d9d2ba53c87f89c5354dd0.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/dnd-session-helper.git/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
